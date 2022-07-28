c852bf7acad8c9e1184141c8a95260481b3f5c27
Patrice <patrice.epplee@gmail.com>
2022-07-28 14:53:46 +0200

    remove particles correctly

---
+++ Particle_Damage.gd
@@ -0,0 +1,10 @@
+extends Spatial
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    yield(get_tree().create_timer(4.0), "timeout")
+    queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#    pass
