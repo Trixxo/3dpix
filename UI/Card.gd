213ec47cbb2e22283475149bc97ec2b638376fe0
Rafael Epplée <hello@rafa.ee>
2022-07-29 20:28:46 +0200

    add card preview

--- UI/Card.gd
+++ UI/Card.gd
@@ -13,6 +13,8 @@ func _process(dt):
     transform = transform.translated(Vector3(dt * 0.5 * sin(3 * sin(idle_movement_factor)), dt * 0.2 * sin(0.3 + idle_movement_factor * 0.5), 0))
     rotate_object_local(Vector3.FORWARD, dt * 0.02 * sin(0.2 + idle_movement_factor))
 
+    $Preview.rotate(Vector3.UP, dt)
+
 func _mouse_entered():
     scale = initial_scale * 1.05
 
