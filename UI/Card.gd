57f90f54c14f41ceea65bce8bf3827de436d6f49
Rafael Epplée <hello@rafa.ee>
2022-07-30 12:26:45 +0200

    hide and show cards

--- UI/Card.gd
+++ UI/Card.gd
@@ -4,6 +4,8 @@ var idle_movement_factor = 0 + randf() * PI
 onready var camera = $'../../Camera'
 onready var initial_scale = scale
 
+signal click
+
 func _ready():
     var _e = $Area.connect("mouse_entered", self, "_mouse_entered")
     var _e2 = $Area.connect("mouse_exited", self, "_mouse_exited")
@@ -24,4 +26,5 @@ func _mouse_exited():
 
 func _area_input_event(_cam, event, _position, _normal, _shape_index):
     if event.is_action_released("game_select"):
+        emit_signal("click")
         $"/root/Node2D/HexGrid".start_building_tower($Preview)
---
+++ UI/UI.gd
@@ -0,0 +1,16 @@
+extends Spatial
+
+func _ready():
+    for card in get_children():
+        card.connect("click", self, "_card_clicked", [card])
+
+func _unhandled_input(event):
+    if event.is_action_released("debug_show_cards"):
+        for card in get_children():
+            card.show()
+
+func _card_clicked(card):
+    $"/root/Node2D/HexGrid".start_building_tower(card.get_node("Preview"))
+    for card in get_children():
+        card.hide()
+
