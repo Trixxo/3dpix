dda017510c1df6e99cfe682b55c9379bf5627b90
Rafael Epplée <hello@rafa.ee>
2022-07-30 13:28:30 +0200

    hide cards at start

--- UI/UI.gd
+++ UI/UI.gd
@@ -3,6 +3,7 @@ extends Spatial
 func _ready():
     for card in get_children():
         card.connect("click", self, "_card_clicked", [card])
+        card.hide()
 
 func _unhandled_input(event):
     if event.is_action_released("debug_show_cards"):
