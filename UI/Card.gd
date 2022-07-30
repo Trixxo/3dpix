2c729d31d49298cf44a3340556ead31f49feef12
Rafael Epplée <hello@rafa.ee>
2022-07-30 19:12:01 +0200

    add card appear animations

--- UI/Card.gd
+++ UI/Card.gd
@@ -1,8 +1,9 @@
 extends MeshInstance
 
-var idle_movement_factor = 0 + randf() * PI
-var idle_movement_speed = 0.4 + randf() * 0.1
+var idle_movement_factor = 0
+var idle_movement_speed = 0.3 + randf() * 0.2
 var preview_node: Spatial
+var is_animating = false
 onready var initial_y = transform.origin.y
 onready var camera = $'../../Camera'
 onready var initial_scale = scale
@@ -13,13 +14,18 @@ func _ready():
     var _e = $Area.connect("mouse_entered", self, "_mouse_entered")
     var _e2 = $Area.connect("mouse_exited", self, "_mouse_exited")
     var _e3 = $Area.connect("input_event", self, "_area_input_event")
+    # prevent flashing of animation "reset" position when first making the cards visible
+    $AnimationPlayer.play("Appear")
+    $AnimationPlayer.advance(0)
+    $AnimationPlayer.stop(false)
 
 func _process(dt):
+    if is_animating or not visible: return
     idle_movement_factor += idle_movement_speed * dt
     transform.origin.x = 0.5 * sin(3 * sin(idle_movement_factor))
-    transform.origin.y = initial_y + sin(0.3 + idle_movement_factor * 0.5)
-    self.rotation_degrees.z = 5 * sin(0.4 + idle_movement_factor * 0.5)
-    self.rotation_degrees.y = -10 + 10 * sin(0.2 + idle_movement_factor)
+    transform.origin.y = initial_y + sin(idle_movement_factor * 0.5)
+    self.rotation_degrees.z = 5 * sin(idle_movement_factor * 0.5)
+    self.rotation_degrees.y = -10 + 10 * cos(idle_movement_factor)
 
 func add_preview(scene: Resource):
     preview_node = scene.instance()
@@ -37,3 +43,25 @@ func _mouse_exited():
 func _area_input_event(_cam, event, _position, _normal, _shape_index):
     if event.is_action_released("game_select"):
         emit_signal("click")
+
+func animate_visibility(visible):
+    is_animating = true
+    if visible:
+        $AnimationPlayer.play("Appear")
+        self.visible = true
+        yield($AnimationPlayer, "animation_finished")
+        is_animating = false
+    else:
+        $AnimationPlayer.play_backwards("Appear")
+        yield($AnimationPlayer, "animation_finished")
+        is_animating = false
+        self.visible = false
+        # reset to match the calculated card transform to the end of the animation
+        reset_transform()
+
+func reset_transform():
+    transform.origin.x = 0
+    transform.origin.y = initial_y
+    self.rotation_degrees.z = 0
+    self.rotation_degrees.y = 0
+    idle_movement_factor = 0
--- UI/UI.gd
+++ UI/UI.gd
@@ -1,19 +1,24 @@
 extends Spatial
 
+var show_cards = false setget set_show_cards
+
 func _ready():
     for card in get_children():
         var type = Towers.random_tower_type()
         card.connect("click", self, "_card_clicked", [card, type])
         card.add_preview(Towers.scene_for_tower(type))
-        card.hide()
+        card.visible = show_cards
 
 func _unhandled_input(event: InputEvent):
     if event.is_action_released("debug_show_cards"):
-        for card in get_children():
-            card.show()
+        self.show_cards = not show_cards
 
 func _card_clicked(card: MeshInstance, tower_type):
     $"/root/Node2D/HexGrid".start_building_tower(card.preview_node, tower_type)
-    for card in get_children():
-        card.hide()
+    self.show_cards = false
 
+func set_show_cards(val):
+    show_cards = val
+    for card in get_children():
+        card.animate_visibility(val)
+        yield(get_tree().create_timer(0.1), "timeout")
