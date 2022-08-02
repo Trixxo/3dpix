e4136eec26c0d86843cb114c92b2185db71b7b6a
Rafael Epplée <hello@rafa.ee>
2022-08-02 15:23:18 +0200

    fix some card animation stuff

--- UI/UI.gd
+++ UI/UI.gd
@@ -1,7 +1,9 @@
 extends Spatial
 
-var experience_needed := 5
+var experience_needed := 1
 var show_cards := false setget set_show_cards
+var is_animating = false
+
 onready var grid = $"/root/Node2D/HexGrid"
 
 func _ready():
@@ -12,10 +14,12 @@ func _ready():
 
 func _unhandled_input(event: InputEvent):
     if event.is_action_released("debug_show_cards"):
+        if not show_cards: shuffle_cards()
         self.show_cards = not show_cards
 
 func _global_vars_updated():
     if GlobalVars.experience >= experience_needed and not show_cards:
+        shuffle_cards()
         self.show_cards = true
         GlobalVars.experience -= experience_needed
 
@@ -25,24 +29,31 @@ func _card_clicked(card: MeshInstance):
     grid.start_building_tower(card.preview_node, card.tower_type)
     self.show_cards = false
 
+# Set a new random upgrade type for each card.
+func shuffle_cards():
+    # check if there are any build options to display
+    var buildable_types = Towers.buildable_types(get_tree().get_nodes_in_group("hexagons"))
+
+    # pick a random build option for each card
+    buildable_types.shuffle()
+    for card in get_children():
+        card.tower_type = buildable_types.pop_back()
+        if card.tower_type == null: continue
+        card.set_preview(Towers.scene_for_tower(card.tower_type))
+
+
 func set_show_cards(val):
+    if is_animating or val == show_cards: return
+    is_animating = true
     show_cards = val
 
-    if show_cards:
-        # check if there are any build options to display
-        var buildable_types = Towers.buildable_types(get_tree().get_nodes_in_group("hexagons"))
-        if buildable_types.empty():
-            show_cards = false
-            return
-
-        # pick a random build option for each card
-        buildable_types.shuffle()
-        for card in get_children():
-            card.tower_type = buildable_types.pop_back()
-            if card.tower_type == null: continue
-            card.set_preview(Towers.scene_for_tower(card.tower_type))
-
-    for card in get_children():
+    var cards = get_children()
+    cards.invert()
+    for card in cards:
         if card.tower_type == null: continue
         card.animate_visibility(val)
         yield(get_tree().create_timer(0.1), "timeout")
+
+    is_animating = false
+    # check if the cards should open again
+    _global_vars_updated()
