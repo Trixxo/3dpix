0fa1be307a0f627f121bd821647b6960382b0703
Rafael Epplée <hello@rafa.ee>
2022-08-04 15:06:05 +0200

    allow building new hexagons

--- HexGrid/HexGrid.gd
+++ HexGrid/HexGrid.gd
@@ -5,9 +5,15 @@ var hexagon_scene = preload("Hexagon.tscn")
 const base_tower_offset = 2.25
 const tower_offset = 1.5
 
+# all relative positions that are directly adjacent to a given hexagon.
+const neighbor_positions = [
+    Vector2(1, 0), Vector2(1, -1), Vector2(0, -1),
+    Vector2(-1, 0), Vector2(-1, 1), Vector2(0, 1)
+]
+
 var cellsize = Vector2(2.3, 2.3)
 var map_size = 1
-# Array of Vector2s containing hex positions
+# Array of Vector2s containing hex positions in axial coordinates
 var hex_map := hexes_in_range(map_size)
 var ground_bounds_top = []
 var ground_bounds_bottom = []
@@ -15,6 +21,8 @@ var building_preview: MeshInstance
 var new_tower_type
 
 func _ready():
+    multimesh.instance_count = 200
+    multimesh.visible_instance_count = 0
     create_hex_meshes_from_cells()
 
 func cell_to_pixel(cell : Vector2) -> Vector2:
@@ -32,22 +40,56 @@ func hexes_in_range(N : int) -> Array:
                 if (q + r + s == 0) and (q != 0 or r != 0 or s != 0):
                     results.append(Vector2(q, r))
     return results
-# Called when the node enters the scene tree for the first time.
+
 func create_hex_meshes_from_cells():
-    multimesh.instance_count = hex_map.size()
     for i in range(hex_map.size()):
         var hex = hex_map[i]
-        var pixe_pos = cell_to_pixel(hex)
-        var freshgon = hexagon_scene.instance()
-        freshgon.transform.origin = Vector3(pixe_pos.x, 0, pixe_pos.y)
-        multimesh.set_instance_transform(i, Transform(Basis().scaled(Vector3.ONE * 2), Vector3(pixe_pos.x, 0, pixe_pos.y)))
-        freshgon.connect("mouse_entered", self, "_mouse_entered_hexagon", [freshgon])
-        freshgon.connect("mouse_exited", self, "_mouse_exited_hexagon")
-        freshgon.connect("input_event", self, "_mouse_clicked_hexagon", [freshgon])
-        add_child(freshgon)
+        var freshgon = create_hexagon(hex)
+        unlock_hexagon(freshgon)
+    
+    # use a duplicate of hex_map here because we're extending hex_map while 
+    # looping over it which would create an infinite loop
+    for position in hex_map.duplicate():
+        create_click_detection_hexagons(position)
+
+
+# For each slot around the given position in which no hexagon exists,
+# create a locked hexagon for detecting clicks.
+# position is in axial coordinates.
+func create_click_detection_hexagons(origin: Vector2):
+    for neighbor_distance in neighbor_positions:
+        var neighbor_position = origin + neighbor_distance
+        # if hexagon already exists, do nothing for this position
+        if neighbor_position in hex_map: continue
+        
+        # create a locked hexagon for this position
+        hex_map.append(neighbor_position)
+        var _freshgon = create_hexagon(neighbor_position)
+
+func create_hexagon(axial_position: Vector2) -> Area:
+    var pixe_pos = cell_to_pixel(axial_position)
+    var freshgon = hexagon_scene.instance()
+    freshgon.axial_position = axial_position
+    freshgon.transform.origin = Vector3(pixe_pos.x, 0, pixe_pos.y)
+    freshgon.connect("mouse_entered", self, "_mouse_entered_hexagon", [freshgon])
+    freshgon.connect("mouse_exited", self, "_mouse_exited_hexagon")
+    freshgon.connect("input_event", self, "_mouse_clicked_hexagon", [freshgon])
+    add_child(freshgon)
+    return freshgon
+
+# make the given hexagon visible.
+func unlock_hexagon(gon):
+    if not gon.is_locked: return 
+
+    multimesh.visible_instance_count += 1
+    var transform = Transform(Basis().scaled(Vector3.ONE * 2), gon.transform.origin)
+    multimesh.set_instance_transform(multimesh.visible_instance_count - 1, transform)
+    gon.is_locked = false
+    create_click_detection_hexagons(gon.axial_position)
 
 func _mouse_entered_hexagon(gon):
-    if (not is_instance_valid(building_preview) 
+    if (not is_instance_valid(building_preview)
+            or gon.is_locked
             or not Towers.can_build_type(new_tower_type, gon.tower_types)): 
         return
 
@@ -61,11 +103,23 @@ func _mouse_exited_hexagon():
     building_preview.hide()
 
 func _mouse_clicked_hexagon(_cam, event, _click_pos, _click_normal, _shape_idx, gon):
-    if (not event.is_action_pressed("game_select") 
-            or not is_instance_valid(building_preview) 
-            or new_tower_type == null
-            or not Towers.can_build_type(new_tower_type, gon.tower_types)):
+    # if this wasn't a mouse click, do nothing
+    if not event.is_action_pressed("game_select"):
+        return
+
+    # if we're not building anything at the moment, try to unlock the hexagon
+    if (not is_instance_valid(building_preview) 
+            or new_tower_type == null):
+
+        if gon.is_locked:
+            unlock_hexagon(gon)
+
         return 
+
+    # if we're selecting a spot to build but are not allowed to build here,
+    # do nothing
+    if not Towers.can_build_type(new_tower_type, gon.tower_types):
+        return
 
     gon.tower_types.append(new_tower_type)
 
--- HexGrid/Hexagon.gd
+++ HexGrid/Hexagon.gd
@@ -1,6 +1,14 @@
 extends Area
 
 var tower_types := []
+var axial_position: Vector2
+# This class is used for raycasting on clicks around existing hexagons.
+# A "locked" hexagon is not visible and not shown in the game 
+# but only exists to see on which position players want to build a new hexagon. 
+var is_locked = true setget set_is_locked
 
-func _ready():
+func set_is_locked(val):
+    is_locked = val
+    # only hexagons that exist from the perspective of the player are added
+    # to this group.
     add_to_group("hexagons")
