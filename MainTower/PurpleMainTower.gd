c4f7dfa1061f00c95b7b0741432464e31d41197c
Patrice <patrice.epplee@gmail.com>
2022-09-30 10:50:56 +0200

    remove comments

--- MainTower/PurpleMainTower.gd
+++ MainTower/PurpleMainTower.gd
@@ -3,7 +3,6 @@ extends Spatial
 
 var cube_projectile_scene = preload("res://Tower/PurpleTower/GreenMainProjectile/Projectile.tscn")
 var experience_sentinel_scene = preload("res://Experience/ExperienceSentinel.tscn")
-var target_velocity
 var _timer
 var attack_speed = GlobalVars.attack_speed
 var origin_rotation
@@ -11,7 +10,6 @@ var origin_transform
 var interpolation_factor = 0
 var height_offset = 0
 var projectiles := 0
-var current_dir := Vector3.ZERO
 var elapsed := 0.0
 
 func _ready():
@@ -37,22 +35,17 @@ func _ready():
 
 func _process(dt):
     elapsed += dt
-    # current_dir = Vector3(0, 0, 1).rotated(Vector3.UP, sin(elapsed))
     transform.basis = Basis().rotated(Vector3.UP, sin(elapsed) * PI / 2.0).rotated(Vector3.UP, PI)
-    # transform = transform.looking_at(current_dir, Vector3.UP)
 
 func _global_vars_updated(all_types, _new_type):
     var bonus_attack_speed := 0.0
     var purple_towers := 0
-    # _timer.set_wait_time(1.0 / GlobalVars.attack_speed)
 
     for type in all_types:
         match Towers.color_for_tower(type):
             Towers.ColorGroup.Purple:
                 purple_towers += 1
     
-    # projectiles = green_towers * 2
-
     _timer.set_wait_time(1.0 / ((GlobalVars.attack_speed * 10.0) + bonus_attack_speed))
 
 func idle_movement(dt):
@@ -60,39 +53,13 @@ func idle_movement(dt):
     rotation.x += 0.3 * dt
                 
 func instance_cube():
-    # for _i in range(projectiles):
     var cube
     var all_enemies = get_tree().get_nodes_in_group("enemies")
     var enemies = ArrayExtra.filter_by_method(all_enemies, "can_attack")
     if enemies.size() > 0:
         cube = cube_projectile_scene.instance()
-        cube.vel = current_dir * cube.speed
-        cube.transform = cube.transform.translated(cube.vel.normalized() * .5)
         cube.target_dir = -transform.basis.z
+        cube.transform = cube.transform.translated(cube.target_dir.normalized() * .5)
         cube.transform.origin.y = height_offset
 
-        target_velocity = cube.vel
-
-        # origin_transform = transform
-        # origin_rotation = transform.basis
-
-        # yield(self, 'finished_interpolation')
         get_tree().get_root().add_child(cube)
-        # target_velocity = null
-    
-# func interpolate_quat(target, dt):
-#     var origin_quat = Quat(origin_rotation)
-#     # var target_transform = Transform.IDENTITY.looking_at(target, Vector3(0, 1, 0))
-#     var target_transform = Transform.IDENTITY.rotated(Vector3(1, 0, 0), PI / 2.0)
-#     # target_transform = target_transform.rotated(target_transform.basis.x, -PI / 2.0)
-#     var target_quat = Quat(target_transform.basis)
-
-#     if ((-transform.basis.z.normalized()).distance_to(target.normalized())) < 0.1:
-#         emit_signal('finished_interpolation')
-#         origin_rotation = transform.basis
-#         interpolation_factor = 0
-#         return Quat(transform.basis)
-#     else:
-#         interpolation_factor += dt * GlobalVars.attack_speed * projectiles * 2.0
-#         interpolation_factor = min(1, interpolation_factor)
-#         return origin_quat.normalized().slerp(target_quat.normalized(), interpolation_factor)
--- Tower/PurpleTower/GreenMainProjectile/PurpleProjectile.gd
+++ Tower/PurpleTower/GreenMainProjectile/PurpleProjectile.gd
@@ -1,100 +1,22 @@
 extends Spatial
 
-onready var init_pos = global_transform[3]
-
-export var max_speed := 40.0
+export var max_speed := 100.0
 var speed_factor := 0.6
 var minimum_speed := 0.9 * max_speed
 var speed := max_speed * speed_factor
 
-var vel := Vector3.ZERO
-var acc := Vector3.ZERO
-var max_steer_force := 10
-var steer_force := 4
-
 var damage = GlobalVars.projectile_damage
-var knockback_force = GlobalVars.knockback_force
-var stun_duration = 0.0
 
 # var target = null setget set_target
 var target_dir := Vector3.ZERO
 
-var enemy_scene = preload("res://Enemy/Enemy.tscn")
-var sphere_expolsion_scene = preload("ParticleDamage.tscn")
-var experience_scene = preload("res://Experience/Experience.tscn")
-
 func _process(dt):
-    # steer_force = pow(speed_factor, 2) * max_steer_force
-    # speed = update_speed()
     if target_dir != Vector3.ZERO:
-
-        # acc += target_dir.normalized() * steer_force
-        # vel += acc * dt
-        # vel = GlobalVars.clamp(vel, speed)
-        
-
-        global_transform = global_transform.translated(Vector3.FORWARD * dt * 20.0)
+        global_transform = global_transform.translated(Vector3.FORWARD * dt * speed)
     
-    # vel = GlobalVars.clamp(vel, speed)
-    # rotation = vel
-    # look_at(Vector3(1, 0, 0), Vector3.UP)
-    # rotation.y = PI / 2.0
-    if (vel.normalized().y != 1):
+    if (target_dir.normalized().y != 1):
         transform.basis = Transform.IDENTITY.looking_at(target_dir.normalized(), Vector3.UP).basis
-        
-    # if target != null:
-    #     var distance_to_target = global_transform[3].distance_to(target)
-    #     # dont move further than the required distance to the target
-    #     var step = min(distance_to_target - 1, vel.length() * dt)
 
     for enemy in get_tree().get_nodes_in_group("enemies"):
         if (enemy.global_transform.origin - global_transform.origin).length() < 1:
             enemy.health -= damage * 5.0
-
-        # if ( distance_to_target <= 4):
-        #     hit_target()
-    # else:
-    #     find_target()
-
-func hit_target(target):
-    # target.play_hit_animation(target.global_transform.origin - global_transform.origin)
-    # target.health -= damage
-    var sphere_explosion = sphere_expolsion_scene.instance()
-    sphere_explosion.transform.origin = target
-    get_tree().get_root().add_child(sphere_explosion)
-    sphere_explosion.get_node("Particles").emitting = true
-    sphere_explosion.get_node("Particles2").emitting = true
-    var vec_to_target = target - global_transform.origin
-    sphere_explosion.rotate_object_local(Vector3(0, 1, 0), atan2(vec_to_target.z, -vec_to_target.x))
-
-    # var poison = poision_scene.instance()
-    # poison.transform.origin = target
-    # poison.transform.origin.y -= .8
-    # get_tree().get_root().add_child(poison)
-
-    # var knockback_dir = Vector3(vec_to_target.x, 0, vec_to_target.z).normalized()
-    # target.global_transform.origin += (knockback_dir * knockback_force)
-    # if stun_duration >= 0.0:
-    #     target.stun(stun_duration)
-
-    # if target.health <= 0:
-    #     var experience = experience_scene.instance()
-    #     experience.transform = target.transform
-    #     get_tree().get_root().add_child(experience)
-    #     target.queue_free()
-
-    target = null
-    queue_free()
-
-class SortMan:
-    static func enemy_sort_dist(a, b):
-        return a.transform.origin.length() < b.transform.origin.length()
-
-func find_target():
-    var enemies = get_tree().get_nodes_in_group("enemies")
-    var enemies_sorted = ArrayExtra.filter_by_method(enemies, "can_attack")
-    enemies_sorted.sort_custom(SortMan, "enemy_sort_dist")
-    if not enemies_sorted.size() > 0: return
-    var first_enemy = enemies_sorted[0]
-    var pos = first_enemy.global_transform.origin
-    self.target = Vector3(pos.x, 0, pos.z)
